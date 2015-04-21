4d0d8ecbc1888c1cf58dfd3e292f5f06eb89b45b
Markus Sähn <nisita8@gmail.com>
2015-04-21 15:38:28 +0200

    add project structure with first modules

---
+++ zwreec/target/doc/search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = {};
+searchIndex['zwreec_bin'] = {"items":[],"paths":[]};
+searchIndex['zwreec'] = {"items":[[0,"","zwreec","",null,null],[5,"compile","","",null,{"inputs":[{"name":"str"},{"name":"str"}],"output":null}],[0,"frontend","","",null,null],[5,"temp_hello","zwreec::frontend","",null,{"inputs":[],"output":{"name":"string"}}],[0,"backend","zwreec","",null,null],[5,"temp_hello","zwreec::backend","",null,{"inputs":[],"output":{"name":"string"}}],[0,"file","zwreec","This is documentation for the `file` module.",null,null],[5,"open_source_file","zwreec::file","Read Text File",null,{"inputs":[{"name":"str"}],"output":{"name":"string"}}],[5,"temp_print_current_directory_files","","",null,{"inputs":[],"output":null}],[5,"temp_hello","","",null,{"inputs":[],"output":{"name":"string"}}]],"paths":[]};
+searchIndex['foo'] = {"items":[],"paths":[]};
+initSearch(searchIndex);
---
+++ zwreec/tests/lib.rs
@@ -0,0 +1,20 @@
+/// extern test-lib for zwreec
+/// to test the whole zwreec library
+/// for the future: real twee and z-code test-data
+
+extern crate zwreec;
+
+#[test]
+fn file_test() {
+    assert_eq!("hello from file", zwreec::file::temp_hello());
+}
+
+#[test]
+fn frontend_test() {
+    assert_eq!("hello from frontend", zwreec::frontend::temp_hello());
+}
+
+#[test]
+fn backend_test() {
+    assert_eq!("hello from backend", zwreec::backend::temp_hello());
+}
