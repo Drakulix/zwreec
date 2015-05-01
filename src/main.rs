b647a797191dd87811f173edacd7c3c340e24db4
Franz Greiling <franz.greiling@fu-berlin.de>
2015-05-01 22:35:11 +0200

    Use getopts to parse command line arguments
    Supported parameters are:
    
    - `-h` display help
    - `-o FILE` specify file name
    
    Also, `-v` to control logging is implemented but has no effect, as the
    log module does not jet support setting the global log level.

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,79 @@ use std::env;
 pub mod utils;
 
 
+// shorthand to display program usage
+macro_rules! print_usage {
+    ($prog:ident, $opts:ident) => {{
+        print!("{}", $opts.usage(&format!("Usage: {} [-h] [-v] [-o OUTPUT] INPUT", $prog)));
+    }}
+}
+
+
 fn main() {
-    log_info!("main started");
+    // set default log level
+    // utils::log::LOG_LEVEL = utils::log::LogLevel::WARN;
 
     // handling commandline parameters
     let args: Vec<String> = env::args().collect();
+    let ref program = args[0];
 
-    let mut input_file_name: &str;
-    let mut output_file_name: &str;
+    // define options
+    let mut opts = getopts::Options::new();
+    opts.optflag("h", "help", "display this help and exit");
+    opts.optflag("v", "verbose", "be more verbose");
+    opts.optopt("o", "", "name of the output file", "FILE");
 
-    match args.len() {
-        1 => {
-            input_file_name = "a.in";
-            output_file_name = "a.out";
-        },
-        3 => {
-            input_file_name = &args[1];
-            output_file_name = &args[2];
-        },
-        _ => {
-            help();
+    let parsed_opts = match opts.parse(&args[1..]) {
+        Ok(m)  => { m }
+        Err(f) => {
+            // parsing error
+            // display usage and return
+            println!("{}", f.to_string());
+            print_usage!(program, opts);
+            // TODO: figure out a way to set exit code
             return;
         }
     };
 
+    // examinate options
+    if parsed_opts.opt_present("h") {
+        // parsed "-h|--help"
+        // display usage and return
+        print_usage!(program, opts);
+        return;
+    }
+
+    if parsed_opts.opt_present("v") {
+        // parsed "-v|--verbose"
+        // set loglevel to verbose
+        // utils::log::LOG_LEVEL = utils::log::LogLevel::VERBOSE;
+    }
+
+    let outfile = if let Some(file) = parsed_opts.opt_str("o") { 
+        // parsed "-o FILE"
+        // set of to filename
+        file
+    } else {
+        // NOTE: string manipulation in rust is stil weird.
+        let mut s = String::new();
+        s.push_str("a.out");
+        s
+    };
+
+    let infile = if parsed_opts.free.len() == 1 {
+         // check number of 'free' parameter
+         // one free parameter is the input file name
+         parsed_opts.free[0].clone()
+    } else {
+        println!("Input file name missing");
+        print_usage!(program, opts);
+        // TODO: figure out a way to set exit code
+        return;
+    };
+
+
     // call library
-    zwreec::compile(input_file_name, output_file_name);
+    zwreec::compile(&infile, &outfile);
 
     // only for testing
     log_verbose!("(1) {}", zwreec::frontend::temp_hello());
@@ -40,9 +87,4 @@ fn main() {
     log_verbose!("(3) {}", zwreec::file::temp_hello());
 
     log_info!("main finished");
-}
-
-fn help() {
-    log_error!("invalid arguments");
-    log_info!("usage:\n    zwreec <input_file> <output_file>");
 }
