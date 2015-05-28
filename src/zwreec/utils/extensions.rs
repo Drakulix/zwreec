ad2c0e2f0bdf8fb5fd34a38d3f419f22438edde2
Drakulix <drakulix@portingteam.com>
2015-05-28 13:09:20 +0200

    Add new Scan-Like Iterator Extension supirior to the stupid Default Scan

--- src/zwreec/utils/extensions.rs
+++ src/zwreec/utils/extensions.rs
@@ -50,6 +50,55 @@ impl<A: Clone, I: Sized+Iterator<Item=A>> PeekingExt for I {
     }
 }
 
+/// An iterator to maintain state while iterating another iterator and allows filtering
+#[must_use = "iterator adaptors are lazy and do nothing unless consumed"]
+#[derive(Clone)]
+pub struct FilteringScan<I, St, F> {
+    iter: I,
+    f: F,
+    state: St,
+}
+
+impl<B, I, St, F> Iterator for FilteringScan<I, St, F> where
+    I: Iterator,
+    F: FnMut(&mut St, I::Item) -> Option<B>,
+{
+    type Item = B;
+
+    #[inline]
+    fn next(&mut self) -> Option<B> {
+        loop {
+            match self.iter.next() {
+                Some(next) => match (self.f)(&mut self.state, next) {
+                    Some(result) => return Some(result),
+                    None => continue,
+                },
+                None => return None,
+            }
+        }
+    }
+
+    #[inline]
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let (_, upper) = self.iter.size_hint();
+        (0, upper) // can't know a lower bound, due to the scan function
+    }
+}
+
+pub trait FilteringScanExt {
+    fn scan_filter<St, B, F>(self, initial_state: St, f: F) -> FilteringScan<Self, St, F>
+        where Self: Sized+Iterator, F: FnMut(&mut St, Self::Item) -> Option<B>;
+}
+
+impl<I: Sized+Iterator> FilteringScanExt for I {
+    fn scan_filter<St, B, F>(self, initial_state: St, f: F) -> FilteringScan<Self, St, F>
+        where Self: Sized+Iterator, F: FnMut(&mut St, I::Item) -> Option<B>,
+    {
+        FilteringScan{iter: self, f: f, state: initial_state}
+    }
+}
+
+
 #[test]
 fn peeking_test() {
     let test = vec!["first", "second", "third"];
@@ -72,4 +121,22 @@ fn peeking_test() {
         }
     }
     assert_eq!(index, 3);
+}
+
+#[test]
+fn filtering_scan_test() {
+    let test = vec![1, 3, 1, 3];
+    let index = 0;
+    let mut sum = 0;
+    for elem in test.into_iter().scan_filter(index, |index, x| {
+        *index += 1;
+        if (*index % 2) == 1 {
+            Some(x)
+        } else {
+            None
+        }
+    }) {
+        sum += elem;
+    }
+    assert_eq!(sum, 2);
 }
