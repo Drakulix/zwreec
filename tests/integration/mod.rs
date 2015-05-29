ad51946eb23a181915fed6195bc1e1c89168d504
Finn Wilke <finn.wilke@fu-berlin.de>
2015-05-29 16:59:56 +0200

    Add simple HelloWorld and ZSCII bold/italics integration tests
    These tests only check that the compiler doesn't crash with specified input
    files. They do not check whether the compiler produces correct output or any
    output at all.

---
+++ tests/integration/mod.rs
@@ -0,0 +1,41 @@
+/// Integration tests for the whole project
+
+extern crate zwreec;
+use std::path::Path;
+use std::fs::File;
+use std::error::Error;
+use std::io::Cursor;
+use std::vec::Vec;
+
+static TESTFOLDER: &'static str = "./tests/integration/sample/";
+
+fn test_compile(input_filename: String) {
+	let path = Path::new(&input_filename);
+    let mut input = match File::open(path) {
+        Err(why) => {
+            panic!("Couldn't open {}: {}",
+                           path.display(), Error::description(&why))
+        },
+        Ok(file) => {
+            file
+        }
+    };
+
+    let vec: Vec<u8> = vec![];
+    let mut output = Cursor::new(vec);
+
+    zwreec::compile(&mut input, &mut output);
+}
+
+#[test]
+fn helloworld_test() {
+	test_compile(TESTFOLDER.to_string() + "HelloWorld.twee");
+}
+
+fn zscii_test() {
+	test_compile(TESTFOLDER.to_string() + "ZSCII.twee");
+}
+
+fn unicode_test() {
+	//test_compile(TESTFOLDER.to_string() + "Unicode.twee");
+}
