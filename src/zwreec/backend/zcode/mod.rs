3066983bd60a15e36329b110ecdab0a999d08bd2
Markus Sähn <nisita8@gmail.com>
2015-05-30 11:43:02 +0200

    -delete comments

--- src/zwreec/backend/zcode/mod.rs
+++ src/zwreec/backend/zcode/mod.rs
@@ -10,43 +10,12 @@ use self::zfile::Zfile;
 
 
 /// an example to show the current status of the z-code implementation
+/// zcode playground function
 pub fn temp_create_zcode_example() {
 
     let mut zfile: Zfile = zfile::Zfile::new();
-    /*zfile.start();
-    zfile.op_call_1n("main");
-    zfile.op_quit();
-
-    zfile.routine("main", 0);
-    zfile.op_set_color(0x02, 0x03);
-    zfile.op_set_text_style(true, false,false,false);
-    zfile.op_print("Z-char 6789abcdef0     1234567\n89abcdef");
-    zfile.op_print_unicode_char(0x24);
-    zfile.op_set_text_style(false, false,false,false);
-    zfile.op_newline();
-    zfile.op_print("current   --------------------------");
-    zfile.op_set_text_style(false, false,false,false);
-    zfile.op_newline();
-    zfile.op_print("A0      abcdefghijklmnop\nqrstuvwxyz");
-    zfile.op_set_text_style(false, true,false,false);
-    zfile.op_newline();
-    zfile.op_print("A1      ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    zfile.op_set_text_style(false, false,false,false);
-    zfile.op_newline();
-    zfile.op_print("A2      ^0123456789.,!?_#'\"/\\-:()");
-    zfile.op_set_text_style(false, false,true,false);
-    zfile.op_newline();
-    zfile.op_print("          --------------------------");
-    zfile.op_newline();
-
-    zfile.op_quit();
-    zfile.end();*/
 
     zfile.start();
-    //zfile.op_inc(0x10);
-    //zfile.op_push_u16(1234);
-    //zfile.op_pull_u16(17);
-    //zfile.op_print_num_var(17);
     zfile.op_call_1n("main");
     zfile.op_quit();
 
