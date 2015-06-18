a5af32ce8b8aa09ed29dd853a5166ec432f88d0f
Franz Greiling <franz.greiling@fu-berlin.de>
2015-06-18 23:51:00 +0200

    Add main library documentation

--- src/zwreec/lib.rs
+++ src/zwreec/lib.rs
@@ -1,5 +1,93 @@
 #![doc(html_root_url="https://drakulix.github.io/zwreec/",
     html_logo_url="https://dl.dropboxusercontent.com/u/70410095/zwreec/logo.png")]
+//! Twee-to-Zcode compile library
+//!
+//! Zwreec is a compiler for [interactive fiction](http://en.wikipedia.org/wiki/Interactive_fiction)
+//! in the Twee format (created by the [Twine software](http://en.wikipedia.org/wiki/Twine_(software))) 
+//! to [Z-Machine](http://en.wikipedia.org/wiki/Z-machine) instructions (Zcode) 
+//! which can be run with interpreters like [frotz](http://frotz.sourceforge.net).
+//!
+//! This library was developed as part of the course "Softwareprojekt 
+//! Übersetzerbau" in 2015.
+//!
+//! # Requirements and Usage
+//!
+//! The library uses a [fork of rustlex](https://github.com/Farthen/rustlex) to
+//! to do lexical analysis. Since rustlex requires the Rust Nightly Builds, so 
+//! does zwreec. More precisely, this library was *only* developed and tested 
+//! against Rust 1.2.0-nightly, build 2015-06-01. You can install this specific 
+//! version using:
+//!
+//! ```sh
+//! $ curl -s https://static.rust-lang.org/rustup.sh | sh -s -- --channel=nightly --date=2015-06-02
+//! ```
+//!
+//! To use zwreec in your project you can add it as a dependency to your `Cargo.toml`.
+//!
+//! ```toml
+//! [dependencies.zwreec]
+//! git = "https:://github.com/Drakulix/zwreec"
+//! ```
+//!
+//! Then you can use it in your crate root.
+//!
+//! ```rust
+//! extern crate zwreec;
+//! ```
+//!
+//! # Example
+//!
+//! The following example is itself a full working command line compiler. It is a simpler
+//! version of the [Reference Binary Implementation](#reference-binary-implementation).
+//!
+//! ```no_run
+//! extern crate zwreec;
+//!
+//! use std::env;
+//! use std::error::Error;
+//! use std::fs::File;
+//! use std::path::Path;
+//!
+//! fn main() {
+//!     let mut args: Vec<String> = env::args().collect();
+//!
+//!     if args.len() != 2 { panic!("Need exactly one input file!"); }
+//!
+//!     let cfg = zwreec::config::Config::default_config();
+//!     let mut input = match File::open(Path::new(&args[1])) {
+//!         Ok(file) => file,
+//!         Err(why) => { panic!("Couldn't open input: {}", Error::description(&why)); }
+//!     };
+//!     let mut output = match File::create(Path::new("a.z8")) {
+//!         Ok(file) => file,
+//!         Err(why) => { panic!("Couldn't open output: {}", Error::description(&why)); }
+//!     };
+//!
+//!     zwreec::compile(cfg, &mut input, &mut output);
+//! }
+//! ```
+//!
+//! # Reference Binary Implementation
+//!
+//! Zwreecs [Github-Repository](https://github.com/Drakulix/zwreec) contains a 
+//! reference binary implementation that uses this library and provides a simple 
+//! command line interface to compile Twee files.
+//!
+//! To build the binary, you need the rust version as outlined in [Requirements and
+//! Usage](#requirements-and-usage). Then you can build the binary using
+//!
+//! ```sh
+//! $ cargo build --release
+//! ```
+//! 
+//! The resulting binary can be found at `target/release/zwreec`. 
+//!
+//! # Logging
+//!
+//! Zwreec uses the logging facade provided by
+//! [log](../log/index.html). The reference binary implementation of zwreec also includes an
+//! implementation of the `Log` trait.
+
 #![feature(plugin)]
 #![plugin(rustlex)]
 #[allow(plugin_as_library)]
