5a744a910e90af4d79745c8834d7a1a08628572a
Franz Greiling <franz.greiling@fu-berlin.de>
2015-06-19 17:19:35 +0200

    Add Documentation to frondend module

--- src/zwreec/frontend/mod.rs
+++ src/zwreec/frontend/mod.rs
@@ -1,3 +1,30 @@
+//! Processes Twee files and generates an AST
+//!
+//! The Frontend is responsible for processing a Twee file by
+//! generating a Token Stream from the input and parsing this stream to build
+//! an Abstract Syntax Tree.
+//!
+//! # Example
+//!
+//! This small example shows how the differend submodules interact and can 
+//! be used to parse a Twee-File.
+//!
+//! ```
+//! # extern crate zwreec;
+//! use std::io::Cursor;
+//!
+//! let cfg = zwreec::config::Config::default_config();
+//! let mut twee = Cursor::new("::Start\nHello World".to_string().into_bytes());
+//!
+//! // Generate Token Stream
+//! let tokens = zwreec::frontend::lexer::lex(&cfg, &mut twee);
+//!
+//! // Parse Tokens
+//! let p = zwreec::frontend::parser::Parser::new(&cfg);
+//! let ast = zwreec::frontend::ast::AST::build(
+//!     p.parse(tokens)
+//! );
+//! ```
 pub mod lexer;
 pub mod parser;
 pub mod ast;
