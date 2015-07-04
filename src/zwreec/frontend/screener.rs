24b97322091f7316fca10452cd7d0d50e33ab44f
Markus Sähn <nisita8@gmail.com>
2015-07-04 14:17:16 +0200

    renamed variable

--- src/zwreec/frontend/screener.rs
+++ src/zwreec/frontend/screener.rs
@@ -3,7 +3,7 @@
 use std::error::Error;
 use std::io::{BufReader,Cursor,Read};
 
-/// checks the input if there is an bom, if true it will delete it
+/// checks the input if there is an BOM, if true it will delete it
 pub fn handle_bom_encoding<'a, R: Read>(input: &'a mut R) -> Cursor<Vec<u8>> {
     let mut reader = BufReader::new(input);
     let mut content = String::new();
@@ -12,23 +12,23 @@ pub fn handle_bom_encoding<'a, R: Read>(input: &'a mut R) -> Cursor<Vec<u8>> {
         Ok(_) => (),
     };
 
-    let mut v: Vec<u8> = content.bytes().collect();
-    if v.len() < 5 {
+    let mut bytes: Vec<u8> = content.bytes().collect();
+    if bytes.len() < 5 {
         error!("The file is to short for a valid twee File");
     }
-    let has_bom = if &v[0..3] == [0xef, 0xbb, 0xbf] {
+    let has_bom = if &bytes[0..3] == [0xef, 0xbb, 0xbf] {
         true
     } else {
         false
     };
     if has_bom {
         debug!("File has Byte order mark");
-        v.remove(0);
-        v.remove(0);
-        v.remove(0);
+        bytes.remove(0);
+        bytes.remove(0);
+        bytes.remove(0);
     }
 
-    let cursor: Cursor<Vec<u8>> = Cursor::new(v);
+    let cursor: Cursor<Vec<u8>> = Cursor::new(bytes);
 
     cursor
 }
